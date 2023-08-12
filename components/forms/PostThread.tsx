25240df92f546577e4c2a4b5dc942efe33b3575c
Urmil7766 <urmil7766@gmail.com>
2023-08-12 17:13:48 +0530

    code fixes

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,76 +1,61 @@
-"use client"
+"use client";
+
+import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useOrganization } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname, useRouter } from "next/navigation";
 
-import { ThreadValidation } from "@/lib/validations/thread";
-import { updateUser } from "@/lib/actions/user.actions";
-import * as z from "zod";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "@/components/ui/form";
- 
-  import { Button } from "@/components/ui/button";
-  import { Textarea } from "@/components/ui/textarea";
-import { createThread } from "@/lib/actions/thread.actions";
-  
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
+import { ThreadValidation } from "@/lib/validations/thread";
+import { createThread } from "@/lib/actions/thread.actions";
 
 interface Props {
-    user: {
-      id: string;
-      objectId: string;
-      username: string;
-      name: string;
-      bio: string;
-      image: string;
-    };
-    btnTitle: string;
-  }
-
- 
-
-
-
-
-function PostThread ({userId} : {userId:string}) {
-    const router = useRouter();
-    const pathname = usePathname();
-      
-  
-      const form = useForm({
-          resolver: zodResolver(ThreadValidation),
-          defaultValues: {
-            thread: "",
-            accountId: userId,
-            },
-        })
-        const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
-    await createThread({
-        text: values.thread,
-        author: userId,
-        communityId: organization ? organization.id : null,
-        path: pathname,
-      });
-  
-      router.push("/");
+  userId: string;
 }
 
+function PostThread({ userId }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
 
+  const { organization } = useOrganization();
 
-    return(
-        <Form {...form}>
-        <form
-          className='mt-10 flex flex-col justify-start gap-10'
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-             <FormField
+  const form = useForm<z.infer<typeof ThreadValidation>>({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: organization ? organization.id : null,
+      path: pathname,
+    });
+
+    router.push("/");
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className='mt-10 flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
           control={form.control}
           name='thread'
           render={({ field }) => (
@@ -85,18 +70,13 @@ function PostThread ({userId} : {userId:string}) {
             </FormItem>
           )}
         />
+
         <Button type='submit' className='bg-primary-500'>
           Post Thread
         </Button>
-
-
-
-
-</form>
-</Form>
-    );
-
-
+      </form>
+    </Form>
+  );
 }
 
 export default PostThread;
